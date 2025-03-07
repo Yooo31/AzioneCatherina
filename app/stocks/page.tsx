f982c4dfd717c8b2abc9b1b6c502e11d75374f15
Yoan <mrts.yoan@gmail.com>
2025-03-07 13:30:34 +0100

    feat(stock2): Add type og stock

--- app/stocks/page.tsx
+++ app/stocks/page.tsx
@@ -8,6 +8,17 @@ export default function StockPage() {
   const { data: session } = useSession();
   const userId = session?.user?.id?.toString() || '';
 
+  const stockTypes = [
+    { value: 'alcool', label: 'Alcool', color: '#E5A300' },
+    { value: 'soft', label: 'Soft', color: '#1100E5' },
+    { value: 'redWine', label: 'Vin rouge', color: '#D40027' },
+    { value: 'whiteWine', label: 'Vin blanc', color: '#FFFF8F' },
+    { value: 'rWine', label: 'Vin rosé', color: '#FF8FD0' }, //! Ajouter VALUE
+    { value: 'rWine', label: 'Surgelé', color: '#27E1F4' }, //! Ajouter VALUE
+    { value: 'rWine', label: 'Sec', color: '#43F427' }, //! Ajouter VALUE
+    { value: 'rWine', label: 'Ustenciles', color: '#000000' }, //! Ajouter VALUE
+  ]
+
   return (
     <main className="p-6 sm:w-full md:w-3/4 lg:w-3/4 m-auto h-full">
       <div className="flex justify-between items-center mb-4">
@@ -15,6 +26,13 @@ export default function StockPage() {
         <StockForm userId={userId} />
       </div>
       <StockTable userId={userId} />
+
+      { stockTypes.map((type) => (
+        <div key={type.value} className="flex items-center gap-2">
+          <span className="w-6 h-6 rounded-full" style={{ backgroundColor: type.color }} />
+          <span>{type.label}</span>
+        </div>
+      ))}
     </main>
   );
 }
